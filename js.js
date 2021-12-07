de5db081eeb064a22483d6249847d6d5fef7eeec
AngieChoque <angienikol321@gmail.com>
2021-12-07 10:16:56 -0500

    mensaje

---
+++ js.js
@@ -0,0 +1,9 @@
+let name = document.getElementById("name");
+let forma = document.getElementById("forma");
+let save_btn = document.getElementById("save-btn");
+let color = document.getElementById("color");
+
+console.log(forma.value);
+console.log(name.value);
+console.log(save_btn.value);
+console.log(color.value);
